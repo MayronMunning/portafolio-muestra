043254d6e2221daea41e86c5178462b662921bd9
Jesús Fernández <crisanfdez@gmail.com>
2024-05-09 21:57:10 -0600

    Agregamos la persistencia de datos, iniciamos con la sección del hero, y usamos una libreria llamada typed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,68 @@
 /* ====================== NAVBAR ====================== */
 const menu = document.getElementById('menu')
-const btnOpen = document.getElementById('btnOpen')
-const btnClose = document.getElementById('btnClose')
+const nav = document.querySelector('.nav')
+const links = nav.querySelectorAll('.menu__link')
 
-function openMenu () {
-  menu.classList.add('show-menu')
+
+nav.addEventListener('click', function (event) {
+  if (event.target.closest('#btnOpen')) {
+    menu.classList.add('show-menu')
+  }
+
+  if (event.target.closest('#btnClose')) {
+    menu.classList.remove('show-menu')
+  }
+
+  if (event.target.matches('.menu__link')) {
+    menu.classList.remove('show-menu')
+
+    for (const link of links) {
+      link.classList.remove('active')
+    }
+
+    event.target.classList.add('active')
+  }
+})
+
+/* =================== Dark Mode =================== */
+const body = document.body
+const ls = window.localStorage
+const btnTheme = document.getElementById('btnTheme')
+
+// Preguntamos si esta activado el darkMode en el localStorage
+const theme = ls.getItem('darkMode')
+const sun = 'bx bxs-sun'
+const moon = 'bx bxs-moon'
+
+if (theme) {
+  body.classList.add('dark')
+  btnTheme.firstElementChild.className = sun
+} else {
+  body.classList.remove('dark')
+  btnTheme.firstElementChild.className = moon
 }
 
-function closeMenu () {
-  menu.classList.remove('show-menu')
-}
+btnTheme.addEventListener('click', function () {
+  body.classList.toggle('dark')
+  if (body.classList.contains('dark')) {
+    // entonces guardo en el localStorage el valor de dark
+    ls.setItem('darkMode', true)
+    btnTheme.firstElementChild.className = sun
+  } else {
+    // entonces voy a eliminar del localStorage el valor de dark
+    ls.removeItem('darkMode')
+    btnTheme.firstElementChild.className = moon
+  }
 
-btnOpen.onclick = openMenu
-btnClose.onclick = closeMenu
-/* =================== Other JS =================== */
+})
+
+/* =================== Typing =================== */
+var typed = new Typed('#typing', {
+  strings: ['Frontend Developer', 'Web Developer', 'Designer', 'Fullstack Developer'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  startDelay: 25,
+  loop: true,
+  loopCount: Infinity,
+  cursorChar: '_',
+})
